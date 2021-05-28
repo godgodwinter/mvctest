75596e6039a7b106aaf4476c61b906a43c920536
godgodwinter <kakadz.pro@gmail.com>
2021-05-28 22:34:59 +0700

    masukkan file laravel dasar dan buat controller view beranda

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,94 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>MVC - TEST</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{{ asset("Arsha/") }}/assets/img/favicon.png" rel="icon">
+  <link href="{{ asset("Arsha/") }}/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{ asset("Arsha/") }}/assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="{{ asset("Arsha/") }}/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="{{ asset("Arsha/") }}/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="{{ asset("Arsha/") }}/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="{{ asset("Arsha/") }}/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="{{ asset("Arsha/") }}/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="{{ asset("Arsha/") }}/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{ asset("Arsha/") }}/assets/css/style.css" rel="stylesheet">
+
+  @yield('csshere')
+  <!-- =======================================================
+  * Template Name: Arsha - v4.1.0
+  * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+
+
+       <!-- ======= Pricing Section ======= -->
+    <section id="harga" class="pricing">
+        <div class="container">
+
+          <div class="section-title">
+            <h2>TEST MVC PHP</h2>
+            <p>Masukkan file quotes.txt</p>
+          </div>
+
+          <div class="row">
+
+            <div class="col-lg-2" >
+
+            </div>
+
+            <div class="col-lg-8 mt-8 mt-lg-0" >
+              <div class="box featured">
+                <h3>Hasil</h3>
+
+                <a href="#team" class="buy-btn">Get Started</a>
+              </div>
+            </div>
+
+            <div class="col-lg-2 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="300">
+
+            </div>
+
+          </div>
+
+        </div>
+      </section><!-- End Pricing Section -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{ asset("Arsha/") }}/assets/vendor/aos/aos.js"></script>
+  <script src="{{ asset("Arsha/") }}/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="{{ asset("Arsha/") }}/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="{{ asset("Arsha/") }}/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="{{ asset("Arsha/") }}/assets/vendor/php-email-form/validate.js"></script>
+  <script src="{{ asset("Arsha/") }}/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="{{ asset("Arsha/") }}/assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{ asset("Arsha/") }}/assets/js/main.js"></script>
+
+</body>
+
+</html>
