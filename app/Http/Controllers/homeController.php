9ecc04fc2259cb7fc9966387fb544ca89aaa6a69
godgodwinter <kakadz.pro@gmail.com>
2021-05-28 19:16:17 +0700

    update tampilkan 1 random dari txt

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -9,114 +9,88 @@ use Illuminate\Support\Facades\DB;
 
 class homeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
 
-        //  //buat array arr
-        //  $arr=array();
+         //buat array arr
+         $arr=array();
 
-        //  // $url=URL::to('/')."/download/quotes.txt";
-        //  $namefile="download/1.txt";
+         // $url=URL::to('/')."/download/quotes.txt";
+         $namefile="download/1.txt";
 
-        //  //open file txt
-        //  $fp = @fopen($namefile, 'r');
+         //open file txt
+         $fp = @fopen($namefile, 'r');
 
-         //get text 1 baris
-         // $line=fgets($fp);
+        //  get text 1 baris
+         $line=fgets($fp);
 
-         //feof cek endoffile
-         // $feof=feof(($fp));
+        //  feof cek endoffile
+         $feof=feof(($fp));
 
-         // ketika bukan end-of-file
-        //      while (!feof($fp))
-        //      {
-        //          $arr_temp=fgets($fp);
-        //          //hapus karakter selain text
-        //          $arr_temp=trim($arr_temp);
-        //          //masukkan ke dalam array
-        //          array_push($arr,$arr_temp);
-        //      }
+        //  ketika bukan end-of-file
+             while (!feof($fp))
+             {
+                 $arr_temp=fgets($fp);
+                 //hapus karakter selain text
+                 $arr_temp=trim($arr_temp);
+                 //masukkan ke dalam array
+                 array_push($arr,$arr_temp);
+             }
 
-        // $datas=$arr;
+        $datas=$arr;
 
-        $datas=array();
+        // $datas=array();
         return view('dashboard',compact('datas'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function reset()
     {
         return redirect('/');
 
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show()
     {
-        dd($i);
-        //ambil jumlah array txt
-         $namefile="download/1.txt";
-
-         //open file txt
-         $fp = @fopen($namefile, 'r');
+        $datas=array();
         $i=0;
+        //buat array arr
+        $arr=array();
 
-        while (!feof($fp))
-        {
-            $i++;
-        }
-dd($i);
+        // $url=URL::to('/')."/download/quotes.txt";
+        $namefile="download/1.txt";
 
-        //random diantara jml
+        //open file txt
+        $fp = @fopen($namefile, 'r');
 
-        //array push to datas
+       //  get text 1 baris
+        $line=fgets($fp);
+
+       //  feof cek endoffile
+        $feof=feof(($fp));
+
+       //  ketika bukan end-of-file
+            while (!feof($fp))
+            {
+                $i++;
+                $arr_temp=fgets($fp);
+                //hapus karakter selain text
+                $arr_temp=trim($arr_temp);
+                //masukkan ke dalam array
+                array_push($arr,$arr_temp);
+            }
+//cari nilai acak
+            $random=rand(0,$i);
+
+    // $tampil=$arr[$random];
+
+array_push($datas,$arr[$random]);
+            // dd($tampil);
+
+    //    $datas=$tampil;
+
+       // $datas=array();
+       return view('dashboard',compact('datas'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
